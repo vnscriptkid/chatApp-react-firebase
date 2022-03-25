eed417d41692cd034aabd506d2f4eac185d4e81d
Thanh Nguyen <vnscriptkid@gmail.com>
2022-03-25 23:27:10 +0700

    refactor(MessageInput): use firebase v9 api

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import {db} from '../firebase';
+import { addDoc, collection, doc } from "@firebase/firestore";
+import React from "react";
+import { db } from "../firebase";
 
 const MessageInput = ({ user, channelId }) => {
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        const inputEle = e.target.elements[0];
-        const nextValue = inputEle.value;
+    const inputEle = e.target.elements[0];
 
-        // TODO: make channel dymamic
-
-        db
-            .collection('channels')
-            .doc(channelId)
-            .collection('messages')
-            .add({
-                text: nextValue,
-                createdAt: Date.now(),
-                userRef: db.collection('users').doc(user.id)
-            })
-            .then(docRef => {})
-            .catch(err => console.error(err));
-
-        // inputEle.value = "";
-        e.target.reset();
+    try {
+      const messagesRef = collection(db, "channels", channelId, "messages");
+      await addDoc(messagesRef, {
+        text: inputEle.value,
+        createdAt: Date.now(),
+        userRef: doc(db, "users", user.id),
+      });
+      inputEle.value = "";
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    return (
-        <form className="messageInputBox" onSubmit={handleSubmit}>
-            <input type="text" className="messageInput" placeholder={`message #${channelId}`} />
-        </form>
-    );
-}
+  return (
+    <form className="messageInputBox" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="messageInput"
+        placeholder={`message #${channelId}`}
+      />
+    </form>
+  );
+};
 
 export default MessageInput;
