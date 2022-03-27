f1825f9f4104826e3086f2d28de27f8bead7e6dc
Thanh Nguyen <vnscriptkid@gmail.com>
2022-03-28 00:19:03 +0700

    feat(functions): cleverbot listens and responds

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+// routes
 exports.helloWorld = require("./routes/hello");
 
+// triggers
 exports.onUserStatusChanged = require("./triggers/onUserStatusChanged");
+exports.onCleverbotMessage = require("./triggers/onCleverbotMessage");
---
+++ functions/triggers/onCleverbotMessage.js
@@ -0,0 +1,39 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const db = admin.firestore();
+
+const bot = {
+  name: "cleverbot",
+  photoUrl: "https://i.imgur.com/ydOMC2c.png",
+  uid: "cleverbot",
+  status: {
+    lastChanged: new Date(),
+    state: "online",
+  },
+  channels: {
+    general: true,
+  },
+};
+
+db.collection("users").doc(bot.uid).set(bot, { merge: true });
+
+module.exports = functions.firestore
+  .document("channels/general/messages/{messageId}")
+  .onCreate((doc, context) => {
+    const message = doc.data();
+
+    if (!message.text.startsWith("@cleverbot")) return;
+
+    const msgFromBot = {
+      text: "hi, can i help u?",
+      userRef: db.collection("users").doc("cleverbot"),
+      createdAt: new Date(),
+    };
+
+    console.log("@@ bot sending msg ", msgFromBot);
+
+    return db
+      .collection("channels/general/messages/{messageId}")
+      .add(msgFromBot);
+  });
